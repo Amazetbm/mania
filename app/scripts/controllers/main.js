8b02bd746864eb4ace965d41474af63ad5b0bbde
Amazetbm <jtalley1@gmail.com>
2017-05-01 22:32:36 -0400

    Adding functions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,6 +22,39 @@ angular.module('movieManiaApp')
     ];
 
     $scope.movies = movieList;
+    $scope.newMovieTitle = '';
+    $scope.newMovieDscription = 'http://www.soulstepradio.com/images/ha/oneok.jpg';
+    $scope.newMovieCategory = '';
+    $scope.newMovieImage = '';
+
+    $scope.validateTitle = function() {
+      if($scope.newMovieTitle.length > 0) {
+        console.debug($scope.newMovieTitle);
+      } else {
+        window.alert('Title is required');
+      }
+    };
+
+    $scope.addMovie = function() {
+      var movie = {
+        title: $scope.newMovieTitle,
+        category: $scope.newMovieCategory,
+        image: $scope.newMovieImage,
+        description: $scope.newMovieDscription
+      };
+
+      $scope.movies.push(movie);
+    };
+
+    $scope.checkCategorySelected = function(){
+      if($scope.newMovieCategory === '') {
+        window.alert('Category cannot be empty');
+      }
+    };
+
+    $scope.checkDescription = function(){
+      console.debug($scope.newMovieDscription);
+    };
   })
   .controller('SubCtrl', function($scope){
     $scope.title = 'Lots of movies: ' + $scope.movies.length + ' movies.';
